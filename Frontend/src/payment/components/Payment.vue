51d3bd3e5cf60d9e946369c62f97931219b9ff6f
Michael Nickel <micha_1997@web.de>
2021-06-18 11:26:22 +0200

    vue initial commit

---
+++ Frontend/src/payment/components/Payment.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Payment</h3>
+    Payment Component is working!
+    <div>{{ test }}</div>
+  </div>
+</template>
+
+<script lang="js">
+import PaymentService from "@/payment/services/payment.service.js"
+export default {
+  name: "Payment",
+  data() {
+    return {
+        test: {},
+    }
+  },
+  async created() {
+    try {
+      const response = await PaymentService.test();
+      console.log(response);
+      this.test = response.data[0];
+    } catch(e) {
+      console.log(e);
+    }
+  }
+};
+</script>
+
+<style>
+h3 {
+  color: #000;
+}
+</style>
---
+++ Frontend/src/views/Payment.vue
@@ -0,0 +1,15 @@
+<template>
+  <Payment />
+</template>
+
+<script>
+// @ is an alias to /src
+import Payment from "@/payment/components/Payment.vue";
+
+export default {
+  name: "PaymentView",
+  components: {
+    Payment,
+  },
+};
+</script>
