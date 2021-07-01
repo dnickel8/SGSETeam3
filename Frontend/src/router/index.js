178f5b09e6dd93daac9e0da6e95921c5071a302b
Lennart Duemke <lennart.duemke@gmail.com>
2021-07-01 18:51:57 +0200

    Changed header.

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -10,22 +10,19 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "Hauptseite",
+    },
   },
   {
     path: "/payment",
     name: "PaymentView",
     component: Payment,
+    meta: {
+      title: "Bezahlung",
+    },
   },
   {
     path: "/cart",
