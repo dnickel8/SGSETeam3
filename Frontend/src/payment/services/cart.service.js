8fed226e46ae3c4e152c2b3042062cf932a71969
Michael Nickel <micha_1997@web.de>
2021-07-09 09:56:17 +0200

    changes

--- Frontend/src/payment/services/cart.service.js
+++ Frontend/src/payment/services/cart.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class CartService {
   constructor() {}
 
-  async placeOrder(articlesToDelete, userId) {
+  async deletePassedArticles(articlesToDelete, userId) {
     return await axios.post(
       `${process.env.VUE_APP_CART_SERVICE_URL}/cart/deletePassedArticles/${userId}`,
       articlesToDelete
--- Frontend/src/payment/services/order.service.js
+++ Frontend/src/payment/services/order.service.js
@@ -9,6 +9,45 @@ class OrderService {
       order
     );
   }
+
+  createOrder(invoice, amount, shippingMethod, address, userId, items) {
+    const order = {
+      date: invoice.invoiceDetails.invoiceDate,
+      user: {
+        id: userId,
+      },
+      products: items,
+      address: {
+        firstName: address.first_name,
+        lastName: address.last_name,
+        street: address.street,
+        number: address.number,
+        postCode: address.code,
+        city: address.city,
+      },
+      shippingAddress: {
+        firstName: invoice.invoiceDetails.recipient.shippingInfo.firstName,
+        lastName: invoice.invoiceDetails.recipient.shippingInfo.surname,
+        street:
+          invoice.invoiceDetails.recipient.shippingInfo.address.street.split(
+            " "
+          )[0],
+        number:
+          invoice.invoiceDetails.recipient.shippingInfo.address.street.split(
+            " "
+          )[1],
+        postCode:
+          invoice.invoiceDetails.recipient.shippingInfo.address.postalCode,
+        city: invoice.invoiceDetails.recipient.shippingInfo.address.city,
+      },
+      shippingMethod: {
+        name: shippingMethod,
+        description: shippingMethod,
+        price: amount.value,
+      },
+    };
+    return order;
+  }
 }
 
 export default new OrderService();
