38b15892444e12ed8ff9ae1507c157fc02ba2771
Yannic Döll <yannic.doell@gmail.com>
2021-07-11 23:13:27 +0200

    Added JSON validation

--- OrderService/product/tests.py
+++ OrderService/product/tests.py
@@ -7,22 +7,51 @@ from django.test.client import RequestFactory
 from bson.objectid import ObjectId
 
 class URLTests(TestCase):
-    '''
+    def setup_databases(self):
+        """Overrides DjangoTestSuiteRunner"""
+        pass
+
+    def teardown_databases(self, *args):
+        """Overrides DjangoTestSuiteRunner"""
+        pass
+    
     def test_place_order_valid_json(self):
         data = {
-            "date": "4.5.2020",
-            "total": 0,
-            "user": {
-                "id": "testUser"
-            },
+            "date": "2021-07-11T19:07:00.831Z",
+            "user": { "id": "6e7c34fe-61f4-4851-ad5c-ee0809acee71" },
             "products": [
                 {
-                "name": "TestProdukt",
-                "image": "https://picsum.photos/50/50",
-                "count": 1,
-                "price": 10
+                "article_name": "Logitech G432",
+                "article_vendor": "Logitech",
+                "article_price": 53.99,
+                "article_url": "60e9da18d77e720014c613da",
+                "article_imagepath": "image_url",
+                "article_count": 1,
+                "article_id": "user:6e7c34fe-61f4-4851-ad5c-ee0809acee71:cart-item:0",
+                "checkbox_value": True
                 }
-            ]
+            ],
+            "address": {
+                "firstName": "asdasd",
+                "lastName": "asdasd",
+                "street": "asdasd",
+                "number": "234",
+                "postCode": "23423",
+                "city": "asdasd"
+            },
+            "shippingAddress": {
+                "firstName": "John",
+                "lastName": "Doe",
+                "street": "ESpachstr.",
+                "number": "1",
+                "postCode": "79111",
+                "city": "Freiburg"
+            },
+            "shippingMethod": {
+                "name": "PayPal",
+                "description": "PayPal",
+                "price": "53.99"
+            }
         }
         response = self.client.post("/api/v1/placeOrder/", json.dumps(data), content_type="application/json")
 
@@ -30,24 +59,47 @@ class URLTests(TestCase):
 
     def test_place_order_invalid_json(self):
         data = {
-            "invalid_date": "4.5.2020",
-            "total": 0,
-            "user": {
-                "id": "testUser"
-            },
+            "invalid": "data",
+            "dasdfte": "2021-07-11T19:07:00.831Z",
+            "user": { "id": "6e7c34fe-61f4-4851-ad5c-ee0809acee71" },
             "products": [
                 {
-                "name": "TestProdukt",
-                "image": "https://picsum.photos/50/50",
-                "count": 1,
-                "price": 10
+                "article_name": "Logitech G432",
+                "article_vendor": "Logitech",
+                "article_price": 53.99,
+                "article_url": "60e9da18d77e720014c613da",
+                "article_imagepath": "image_url",
+                "article_count": 1,
+                "article_id": "user:6e7c34fe-61f4-4851-ad5c-ee0809acee71:cart-item:0",
+                "checkbox_value": True
                 }
-            ]
+            ],
+            "address": {
+                "firstName": "asdasd",
+                "lastName": "asdasd",
+                "street": "asdasd",
+                "number": "234",
+                "postCode": "23423",
+                "city": "asdasd"
+            },
+            "shippingAddress": {
+                "firstName": "John",
+                "lastName": "Doe",
+                "street": "ESpachstr.",
+                "number": "1",
+                "postCode": "79111",
+                "city": "Freiburg"
+            },
+            "shippingMethod": {
+                "name": "PayPal",
+                "description": "PayPal",
+                "price": "53.99"
+            }
         }
         response = self.client.post("/api/v1/placeOrder/", json.dumps(data), content_type="application/json")
 
         self.assertEquals(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-    '''
+
     def test_get_orders_existing_user(self):
         response = self.client.get('/api/v1/getOrders/123/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
