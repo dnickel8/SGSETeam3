f3c0751035d6c9d63f0b5762af8a342108ae3e1e
Yannic Döll <yannic.doell@gmail.com>
2021-07-11 20:37:32 +0200

    Refactor history

--- Frontend/src/order_service/views/History.vue
+++ Frontend/src/order_service/views/History.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="d-flex justify-center mb-6">
+    <div v-if="showNoContentMessage" class="text-h6 mb-8">
+      Keine Bestellungen vorhanden
+    </div>
     <v-expansion-panels>
       <v-expansion-panel v-for="(order, index) in orders" :key="index">
         <v-expansion-panel-header>
@@ -19,11 +22,11 @@
               <v-img
                 max-height="50"
                 max-width="50"
-                v-bind:src="product.image"
+                v-bind:src="product.article_imagepath"
               ></v-img>
-              <strong class="pa-2">{{ product.name }}</strong>
-              <strong class="pa-2">{{ product.count }}</strong>
-              <strong class="pa-2">{{ product.price }}€ </strong>
+              <strong class="pa-2">{{ product.article_name }}</strong>
+              <strong class="pa-2">{{ product.article_count }}</strong>
+              <strong class="pa-2">{{ product.article_price }}€ </strong>
             </v-card>
           </div>
 
@@ -33,9 +36,6 @@
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-    <div v-if="showNoContentMessage" class="text-h6 mb-8">
-      Keine Bestellungen vorhanden
-    </div>
   </div>
 </template>
 
@@ -78,10 +78,28 @@ export default {
         })
         .catch(() => {});
     },
+    async getImage(image_url) {
+      return await this.axios.get(image_url);
+    },
+    async getAllImages() {
+      for (let o = 0; o < this.orders.length; ++o) {
+        for (let i = 0; i < this.orders[o].products.length; ++i) {
+          let image = await this.getImage(
+            this.orders[o].products[i].article_imagepath
+          );
+          if (image) {
+            this.orders[o].products[i].article_imagepath = image.data.data;
+          } else {
+            this.orders[o].products[i].article_imagepath = "ERROR";
+          }
+        }
+      }
+    },
   },
   async mounted() {
     this.user_id = this.$store.state.userId;
-    this.getOrders();
+    await this.getOrders();
+    await this.getAllImages();
   },
 };
 </script>
